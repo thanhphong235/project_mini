6f8eaa2079af800946940662566b00d33da7367d
nguyễn phong <thanhphong23502@gmail.com>
2025-10-28 21:56:02 +0700

    Fix: refactor OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,57 +1,45 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # Google OAuth2
   def google_oauth2
-    handle_google_oauth
+    handle_oauth("Google")
   end
 
+  # Facebook OAuth
   def facebook
-    handle_auth("Facebook")
+    handle_oauth("Facebook")
   end
 
+  # Twitter OAuth
   def twitter
-    handle_auth("Twitter")
+    handle_oauth("Twitter")
   end
 
+  # Khi OAuth thất bại
   def failure
     redirect_to root_path, alert: "Đăng nhập thất bại, vui lòng thử lại."
   end
 
   private
 
-  # Riêng Google, đảm bảo admin được update uid
-  def handle_google_oauth
+  # Xử lý chung cho tất cả OAuth
+  def handle_oauth(kind)
     auth = request.env["omniauth.auth"]
 
-    # Tìm user theo email (cả admin và user thường)
+    # Tìm user theo email (hoặc provider+uid nếu cần)
     user = User.find_by(email: auth.info.email)
 
     if user
-      # Cập nhật provider và uid từ Google
+      # Cập nhật provider/uid
       user.update(provider: auth.provider, uid: auth.uid)
-
-      # Đăng nhập
-      sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
-      # Nếu chưa có user thì tạo mới
+      # Tạo mới user nếu chưa có
       user = User.from_omniauth(auth)
-      if user.persisted?
-        sign_in_and_redirect user, event: :authentication
-        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-      else
-        session["devise.google_data"] = auth.except("extra")
-        redirect_to new_user_registration_url, alert: "Đăng nhập bằng Google thất bại."
-      end
     end
-  end
 
-  # Xử lý chung cho Facebook/Twitter
-  def handle_auth(kind)
-    auth = request.env['omniauth.auth']
-    @user = User.from_omniauth(auth)
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+    if user.persisted?
+      sign_in(user, event: :authentication)
       set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+      # Không redirect ở đây, Devise sẽ dùng after_sign_in_path_for
     else
       session["devise.#{kind.downcase}_data"] = auth.except("extra")
       redirect_to new_user_registration_url, alert: "Đăng nhập bằng #{kind} thất bại."

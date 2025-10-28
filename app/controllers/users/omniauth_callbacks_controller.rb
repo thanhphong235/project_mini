130601f98015c4e6ea1e63d24be6736c3de97c12
nguyễn phong <thanhphong23502@gmail.com>
2025-10-28 22:02:55 +0700

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,48 +1,43 @@
+# app/controllers/users/omniauth_callbacks_controller.rb
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # Google OAuth2
+  # Google callback
   def google_oauth2
     handle_oauth("Google")
   end
 
-  # Facebook OAuth
+  # Facebook callback
   def facebook
     handle_oauth("Facebook")
   end
 
-  # Twitter OAuth
+  # Twitter callback
   def twitter
     handle_oauth("Twitter")
   end
 
-  # Khi OAuth thất bại
+  # Nếu login thất bại
   def failure
     redirect_to root_path, alert: "Đăng nhập thất bại, vui lòng thử lại."
   end
 
   private
 
-  # Xử lý chung cho tất cả OAuth
-  def handle_oauth(kind)
+  def handle_oauth(provider_name)
     auth = request.env["omniauth.auth"]
 
-    # Tìm user theo email (hoặc provider+uid nếu cần)
-    user = User.find_by(email: auth.info.email)
+    # Tìm user theo email nếu có (Google/Facebook), hoặc tạo mới
+    user = User.find_by(email: auth.info.email) if auth.info.email.present?
 
-    if user
-      # Cập nhật provider/uid
-      user.update(provider: auth.provider, uid: auth.uid)
-    else
-      # Tạo mới user nếu chưa có
-      user = User.from_omniauth(auth)
-    end
+    user ||= User.from_omniauth(auth)
 
     if user.persisted?
-      sign_in(user, event: :authentication)
-      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
-      # Không redirect ở đây, Devise sẽ dùng after_sign_in_path_for
+      # Đăng nhập user và redirect theo after_sign_in_path_for
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: provider_name) if is_navigational_format?
     else
-      session["devise.#{kind.downcase}_data"] = auth.except("extra")
-      redirect_to new_user_registration_url, alert: "Đăng nhập bằng #{kind} thất bại."
+      # Nếu user không save được, lưu dữ liệu tạm và redirect đến đăng ký
+      session["devise.#{provider_name.downcase}_data"] = auth.except("extra")
+      redirect_to new_user_registration_url, alert: "Đăng nhập bằng #{provider_name} thất bại."
     end
   end
 end

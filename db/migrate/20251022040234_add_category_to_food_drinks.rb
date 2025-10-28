178ef1811df2729d1e8f1ecea56a8f8e15766dc2
nguyễn phong <thanhphong23502@gmail.com>
2025-10-28 21:41:44 +0700

    Fix typo ign_key -> foreign_key in migration

--- db/migrate/20251022040234_add_category_to_food_drinks.rb
+++ db/migrate/20251022040234_add_category_to_food_drinks.rb
@@ -1,5 +1,5 @@
 class AddCategoryToFoodDrinks < ActiveRecord::Migration[7.1]
   def change
-    add_reference :food_drinks, :category, null: false,ign_key: true
+    add_reference :food_drinks, :category, null: false, foreign_key: true
   end
 end

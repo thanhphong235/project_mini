b602ed9d0debcc5b00705feb54aecc59818631e9
nguyễn phong <thanhphong23502@gmail.com>
2025-10-29 14:11:31 +0700

    wwww

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,3 @@
+Rails.application.config.active_storage.queues.analysis = :inline
+Rails.application.config.active_storage.queues.purge    = :inline
+Rails.application.config.active_storage.queues.mirror   = :inline

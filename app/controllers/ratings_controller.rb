6a80822d553c8f8505f2a238f8f5247a19fa1ce3
nguyễn phong <thanhphong23502@gmail.com>
2025-11-10 13:43:49 +0700

    done-rate

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,7 @@
 class RatingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_food_drink, only: [:new, :create]
+  before_action :set_food_drink
   before_action :set_rating, only: [:update, :destroy]
-
-  # GET /food_drinks/:food_drink_id/ratings/new
-  def new
-    @rating = @food_drink.ratings.find_or_initialize_by(user: current_user)
-    render partial: "ratings/form", locals: { rating: @rating, food_drink: @food_drink }
-  end
 
   # POST /food_drinks/:food_drink_id/ratings
   def create
@@ -15,51 +9,49 @@ class RatingsController < ApplicationController
     @rating.assign_attributes(rating_params)
 
     if @rating.save
-      flash.now[:notice] = "Đánh giá đã được gửi!"
+      render_turbo_stream
     else
-      flash.now[:alert] = @rating.errors.full_messages.to_sentence
-    end
-
-    respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_streams_for(@rating) }
-      format.html { redirect_to food_drink_path(@food_drink) }
+      render :new
     end
   end
 
-  # PATCH /ratings/:id
+  # PATCH/PUT /food_drinks/:food_drink_id/ratings/:id
   def update
-    if @rating.update(rating_params)
-      flash.now[:notice] = "Đánh giá đã được cập nhật!"
-    else
-      flash.now[:alert] = @rating.errors.full_messages.to_sentence
-    end
+    @rating.assign_attributes(rating_params)
 
-    respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_streams_for(@rating) }
-      format.html { redirect_to food_drink_path(@rating.food_drink) }
+    if @rating.save
+      render_turbo_stream
+    else
+      render :edit
     end
   end
 
-  # DELETE /ratings/:id
+  # DELETE /food_drinks/:food_drink_id/ratings/:id
   def destroy
-    food_drink = @rating.food_drink
+    @rating = @food_drink.ratings.find(params[:id])
     @rating.destroy
-    flash.now[:notice] = "Đánh giá đã được xóa!"
-
-    # Tạo rating rỗng cho form
-    new_rating = food_drink.ratings.find_or_initialize_by(user: current_user)
 
     respond_to do |format|
       format.turbo_stream do
+        rating = @food_drink.ratings.find_or_initialize_by(user: current_user)
         render turbo_stream: [
-          turbo_stream.replace("user_rating_form", partial: "ratings/form", locals: { rating: new_rating, food_drink: food_drink }),
-          turbo_stream.replace("ratings_list", partial: "ratings/list", locals: { ratings: food_drink.ratings.includes(:user) }),
-          turbo_stream.replace("flash_messages", partial: "shared/flash")
+          turbo_stream.replace(
+            "user_rating_form",
+            partial: "ratings/form",
+            locals: { rating: rating, food_drink: @food_drink }
+          ),
+          turbo_stream.replace("ratings_list", partial: "ratings/list", locals: { ratings: @food_drink.ratings.includes(:user) }),
+          turbo_stream.replace("flash_messages", partial: "shared/flash"),
+          # thêm line này để gọi init JS sau khi frame replace
+          turbo_stream.append(
+            "user_rating_form",
+            "<script>initStarRating(document.querySelector('#user_rating_form'))</script>".html_safe
+          )
         ]
       end
-      format.html { redirect_to food_drink_path(food_drink) }
     end
   end
+
 
   private
 
@@ -68,20 +60,23 @@ class RatingsController < ApplicationController
   end
 
   def set_rating
-    @rating = Rating.find(params[:id])
+    @rating = @food_drink.ratings.find(params[:id])
   end
 
   def rating_params
     params.require(:rating).permit(:score, :comment)
   end
 
-  # Turbo stream helper
-  def turbo_streams_for(rating)
-    food_drink = rating.food_drink
-    [
-      turbo_stream.replace("user_rating_form", partial: "ratings/form", locals: { rating: rating, food_drink: food_drink }),
-      turbo_stream.replace("ratings_list", partial: "ratings/list", locals: { ratings: food_drink.ratings.includes(:user) }),
-      turbo_stream.replace("flash_messages", partial: "shared/flash")
-    ]
+  # Gửi turbo_stream để cập nhật form, danh sách ratings và flash
+  def render_turbo_stream
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace("user_rating_form", partial: "ratings/form", locals: { rating: @rating, food_drink: @food_drink }),
+          turbo_stream.replace("ratings_list", partial: "ratings/list", locals: { ratings: @food_drink.ratings.includes(:user) }),
+          turbo_stream.replace("flash_messages", partial: "shared/flash")
+        ]
+      end
+    end
   end
 end

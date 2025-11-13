305c726ab8aa17266519f421430ffa63d0614129
nguyễn phong <thanhphong23502@gmail.com>
2025-11-13 14:03:59 +0700

    Auto run seed on deploy if empty

--- config/application.rb
+++ config/application.rb
@@ -1,33 +1,17 @@
-require_relative "boot"
-
-require "rails/all"
-
-# Load .env trước khi Rails config
-require "dotenv"
-Dotenv.load(".env")
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
 module FoodsDrinksApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
     config.time_zone = "Hanoi"
     config.active_record.default_timezone = :local
 
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # ⚙️ Auto-run db:seed on first boot (only if empty)
+    config.after_initialize do
+      if FoodDrink.count.zero?
+        Rails.logger.info "⚙️ Auto running db:seed (no FoodDrink data found)"
+        Rails.application.load_seed
+      end
+    end
   end
 end

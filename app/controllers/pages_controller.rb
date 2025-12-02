3df5f7a24fce0e43f70e01803973a094da0695b2
nguyễn phong <thanhphong23502@gmail.com>
2025-12-02 11:27:46 +0700

    done 50% theme user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @categories = Category.all # Food, Drink…
+  @selected_category = params[:category_id]
+
+  @food_drinks = FoodDrink.all
+  @food_drinks = @food_drinks.where(category_id: @selected_category) if @selected_category.present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'menus/index'
   get 'contacts/index'
   get 'gallerys/index'
   get 'events/index'
@@ -77,6 +78,7 @@ Rails.application.routes.draw do
 
   #Contacts
   resources :contacts, only: [:index, :create]
+
 
   # Đơn hàng (USER)
   resources :orders, only: [:index, :show, :update, :create] do

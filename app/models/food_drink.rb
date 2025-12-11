28509511d5861f32043665fbbb162e1fab008759
nguyễn phong <thanhphong23502@gmail.com>
2025-12-11 10:30:22 +0700

    fix ratting

--- app/models/food_drink.rb
+++ app/models/food_drink.rb
@@ -11,4 +11,9 @@ class FoodDrink < ApplicationRecord
   has_many :cart_items
   has_many :order_items
   has_many :orders, through: :order_items
+
+  def average_rating
+  ratings.average(:score)&.round(1) || 0
+end
+
 end
